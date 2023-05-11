2a5cb2f795941ee59a5dbdeec1dc0f69f107eab9
Henok <henokmicrometre@gamil.com>
2023-05-11 21:07:52 +0100

    testing astro

---
+++ src/components/SwiperHero/SwiperHero.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function SwiperHero({ children }) {
+  return (
+    <>
+    <button>{children}</button>
+          <div>
+        <div
+          id="heroCarousel"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="/images/carousel/carousel.webp" className="img-fluid" alt="laptop " loading="eager"/>
+            </div>
+            <div className="carousel-item ">
+              <img src="/images/carousel/carousel3.webp" className="img-fluid" alt="server " loading="eager" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SwiperHero;
